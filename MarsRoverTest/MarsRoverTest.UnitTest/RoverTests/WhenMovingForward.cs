09faf3a5d91ddbd7683268c0d3d49892a2e77e4d
Brandon Miller <bmiller340@gmail.com>
2020-04-17 01:16:45 -0400

    Moving Backward Implemented

--- MarsRoverTest/MarsRoverTest.UnitTest/RoverTests/WhenMovingForward.cs
+++ MarsRoverTest/MarsRoverTest.UnitTest/RoverTests/WhenMovingForward.cs
@@ -50,5 +50,52 @@ namespace MarsRoverTest.UnitTest.RoverTests
             Assert.AreEqual(-1, rover.X);
         }
 
+
+
+    }
+
+    [TestClass]
+    public class WhenMovingBackward
+    {
+        [TestMethod]
+        public void AndFacingNorthThenYDecreasesByOne()
+        {
+            //arrange 
+            var rover = new Rover { Direction = Direction.North };
+            //act
+            rover.MoveBackward();
+            //assert
+            Assert.AreEqual(-1, rover.Y);
+        }
+        [TestMethod]
+        public void AndFacingSouthThenYIncreasesByOne()
+        {
+            //arrange 
+            var rover = new Rover { Direction = Direction.South };
+            //act
+            rover.MoveBackward();
+            //assert
+            Assert.AreEqual(1, rover.Y);
+        }
+        [TestMethod]
+        public void AndFacingEastThenXDecreasesByOne()
+        {
+            //arrange 
+            var rover = new Rover { Direction = Direction.East };
+            //act
+            rover.MoveBackward();
+            //assert
+            Assert.AreEqual(-1, rover.X);
+        }
+        [TestMethod]
+        public void AndFacingWestThenXIncreasesByOne()
+        {
+            //arrange 
+            var rover = new Rover { Direction = Direction.West };
+            //act
+            rover.MoveBackward();
+            //assert
+            Assert.AreEqual(1, rover.X);
+        }
     }
 }
--- MarsRoverTest/MarsRoverTest/Rover.cs
+++ MarsRoverTest/MarsRoverTest/Rover.cs
@@ -38,7 +38,25 @@ namespace MarsRover
                 X = X - 1;
             }
         }
-        // Write moving forward for each direction south = y -1; east x +1; west x-1;
+        public void MoveBackward()
+        {
+            if (Direction == Direction.North)
+            {
+                Y = Y - 1;
+            }
+            else if (Direction == Direction.South)
+            {
+                Y = Y + 1;
+            }
+            else if (Direction == Direction.East)
+            {
+                X = X - 1;
+            }
+            else if (Direction == Direction.West)
+            {
+                X = X + 1;
+            }
+        }
     }
 }
 

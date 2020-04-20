0e4d49c7b7768093241d16ad160faeeb81fa981a
bmillss <40220043+bmillss@users.noreply.github.com>
2020-04-20 12:33:57 -0400

    Feature/moving backward (#5)
    * Updated based on all PR comments
    
    * Moving Backward Implemented
    
    * Added feature when moving backward

--- MarsRoverTest/MarsRoverTest.UnitTest/RoverTests/WhenMovingBackward.cs
+++ MarsRoverTest/MarsRoverTest.UnitTest/RoverTests/WhenMovingBackward.cs
@@ -1,56 +1,50 @@
 ﻿using MarsRover;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MarsRoverTest.UnitTest.RoverTests
 {
     [TestClass]
-    public class WhenMovingForward
+    public class WhenMovingBackward
     {
         [TestMethod]
-        public void AndFacingNorthThenYIncrementsByOne()
+        public void AndFacingNorthThenYDecreasesByOne()
         {
             //arrange 
             var rover = new Rover { Direction = Direction.North };
             //act
-            rover.MoveForward();
-            //assert
-            Assert.AreEqual(1, rover.Y);
-        }
-        [TestMethod]
-        public void AndFacingSouthThenYDecreasesByOne()
-        {
-            //arrange 
-            var rover = new Rover { Direction = Direction.South };
-            //act
-            rover.MoveForward();
+            rover.MoveBackward();
             //assert
             Assert.AreEqual(-1, rover.Y);
         }
         [TestMethod]
-        public void AndFacingEastThenXIncrementsByOne()
+        public void AndFacingSouthThenYIncreasesByOne()
+        {
+            //arrange 
+            var rover = new Rover { Direction = Direction.South };
+            //act
+            rover.MoveBackward();
+            //assert
+            Assert.AreEqual(1, rover.Y);
+        }
+        [TestMethod]
+        public void AndFacingEastThenXDecreasesByOne()
         {
             //arrange 
             var rover = new Rover { Direction = Direction.East };
             //act
-            rover.MoveForward();
+            rover.MoveBackward();
             //assert
-            Assert.AreEqual(1, rover.X);
+            Assert.AreEqual(-1, rover.X);
         }
         [TestMethod]
-        public void AndFacingWestThenXDecreasesByOne()
+        public void AndFacingWestThenXIncreasesByOne()
         {
             //arrange 
             var rover = new Rover { Direction = Direction.West };
             //act
-            rover.MoveForward();
+            rover.MoveBackward();
             //assert
-            Assert.AreEqual(-1, rover.X);
+            Assert.AreEqual(1, rover.X);
         }
-
-
-
     }
 }
